ef261ed9aa49f5b421cfe05e24919e0445499796
ymsht <git@ymsht.info>
2020-07-26 13:06:04 +0900

    設定修正

--- vue.config.js
+++ vue.config.js
@@ -5,17 +5,14 @@ module.exports = {
   configureWebpack: {
     performance: {
       hints: false,
-    }
+    },
+    output: {
+      filename: '[name].[hash].js',
+    },
   },
   devServer: {
     host: '0.0.0.0',
     hot: true,
     disableHostCheck: true,
-  },
-  configureWebpack: {
-    output: {
-      filename: '[name].[hash].js',
-    },
-  }
-  
+  },  
 }
