95afbdc4fec1aa44d44374945d34c22964145fa6
ymsht <git@ymsht.info>
2020-07-26 19:05:16 +0900

    ポート変更

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,9 @@ module.exports = {
   },
   devServer: {
     host: '0.0.0.0',
+    port: 8080,
     hot: true,
-    public: '0.0.0.0:80',
+    public: '0.0.0.0:8080',
     disableHostCheck: true,
   },  
 }
