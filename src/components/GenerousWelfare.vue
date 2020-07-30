456328839eed91d2e1a6e84709e9cda7d00c5cfe
ymsht <git@ymsht.info>
2020-07-30 21:13:01 +0900

    構文修正

--- src/components/GenerousWelfare.vue
+++ src/components/GenerousWelfare.vue
@@ -1,7 +1,9 @@
 <template>
   <v-list>
     <v-list-item v-for="(generousWelfare, i) in generousWelfareArray" :key="generousWelfare.id" @click="filter">
-      <v-checkbox @click="checked" :value="generousWelfare.id" v-model="checked[i]"></v-checkbox>
+      <v-list-item-action>
+            <v-checkbox :value="generousWelfare.id" v-model="checked[i]"></v-checkbox>
+      </v-list-item-action>
       <v-list-item-title>{{ generousWelfare.name }}</v-list-item-title>
     </v-list-item>
   </v-list>
